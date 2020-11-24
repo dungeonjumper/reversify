1ce9c8bf1909bbdbc944ca2d6db2ae62d413bffa
April Duangjumpa <april.duangjumpa@milespartnership.com>
2020-11-23 23:54:24 -0500

    Add SASS compiling to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,32 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/site.js',
+  entry: [
+    './src/site.js',
+    './src/style.scss'
+  ],
   output: {
-    filename: 'site.bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/site.bundle.js',
+    path: path.resolve(__dirname, './dist'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [],
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: { outputPath: 'css/', name: '[name].min.css'}
+          },
+          'sass-loader'
+        ],
+      }
+    ]
+  }
 };
